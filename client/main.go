dd15bda18a80c7884690221137f802adf1573c29
MazieroMarco <maziero.marco@gmail.com>
2021-10-07 16:08:26 +0200

    Added base code alowing server and client to communicate
    Signed-off-by: MazieroMarco <maziero.marco@gmail.com>

--- client/main.go
+++ client/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
 
+/**
+Entry point for the client app
+ */
+func main() {
+	// Connects to the server on localhost:8000
+	socket, err := net.Dial("tcp", "localhost:8000")
+
+	// Error handle
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer socket.Close()
+
+	// Copies the os input into the created socket
+	endOfComm := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, socket)
+		log.Println("done")
+		endOfComm <- struct{}{}
+	}()
+
+	// Copies the received data from the socket to os input
+	if _, err := io.Copy(socket, os.Stdin); err != nil {
+		log.Fatal(err)
+	}
+
+	// Closes the socket and waits for goroutine to finish
+	socket.Close()
+	<-endOfComm
 }
