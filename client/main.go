a1ae2787ae16e062a801498064010fe19bdae53d
Gaétan Zwick <gaetan.zwick@heig-vd.ch>
2021-10-24 13:30:40 +0200

    fjdsoifsd

--- client/main.go
+++ client/main.go
@@ -1,42 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"hotel/cmd"
 	"log"
 	"net"
 	"os"
-	"strconv"
 	"strings"
 )
 
-func readLineUntil(str string, conn net.Conn) {
-	for !strings.Contains(readLine(conn), str) {}
-}
-
-func readLine(conn net.Conn) string {
-	reply := make([]byte, 1024)
-	_, err := conn.Read(reply)
-	if err != nil {
-		println("Read to server failed:", err.Error())
-		os.Exit(1)
+//func readLineUntil(str string, conn net.Conn) string { // TODO check this return
+func readLineUntil(str string, scanner* bufio.Scanner) string { // TODO check this return
+	reply := readLine(scanner)
+	for !strings.Contains(reply, str) {
+		reply += readLine(scanner)
 	}
-	println(string(reply))
-	return string(reply)
+	return reply
 }
 
-func writeStr(str string, conn net.Conn) {
-	_, err := fmt.Fprint(conn, str)
+//func readLine(conn net.Conn) string {
+func readLine(scanner* bufio.Scanner) string {
+	//reply := make([]byte, 1024)
+	//_, err := conn.Read(reply)
+	scanner.Scan()
+	return scanner.Text()
+	//if err != nil {
+	//	println("Read to server failed:", err.Error())
+	//	os.Exit(1)
+	//}
+	//return string(reply)
+}
+
+func writelnStr(str string, conn* net.Conn) {
+	_, err := fmt.Println(*conn, str)
 	if err != nil {
 		println("Write to server failed:", err.Error())
 		os.Exit(1)
 	}
 }
 
-func writelnStr(str string, conn net.Conn) {
-	writeStr(str + "\n", conn)
-}
-
 func Connect() net.Conn {
+	// Connects to the hotel on localhost:8000
 	conn, err := net.Dial("tcp", "localhost:8000")
 	if err != nil {
 		log.Fatal(err)
@@ -55,36 +60,30 @@ func Disconnect(conn net.Conn) {
 Entry point for the client app
 */
 func main() {
-	// Connects to the hotel on localhost:8000
-	conn, err := net.Dial("tcp", "localhost:8000")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func(conn net.Conn) {
-		err := conn.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}(conn)
+	conn := Connect()
+	defer Disconnect(conn)
 
-	tests := []Test{
-		{"BOOK 1 1 1", "OK votre chambre a été réservée"},
-		{"BOOK 1 1 1", "ERR votre chambre est déjà réservée"},
-	}
+	consoleReader := bufio.NewReader(os.Stdin)
+	//fmt.Print("Entrez votre nom: ")
+	//username, err := consoleReader.ReadString('\n')
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//fmt.Println("Bonjour " + username + ", entrez vos requêtes: ")
 
-	readLineUntil("Veuillez spécifier votre nom :", conn)
-	writelnStr("UserTest_1", conn)
-	readLineUntil(">> ", conn)
+	var req string
+	var reply string
 
-	for index, test := range tests {
-		println(test.query)
-		writelnStr(test.query, conn)
-		reply := readLine(conn)
-		println("responses : " + reply)
-		if !strings.Contains(reply , test.response) {
-			println("test number " + strconv.Itoa(index) + " failed")
-		} else {
-			println("test number " + strconv.Itoa(index) + " success")
-		}
+	scanner := bufio.NewScanner(conn)
+
+	reply = readLineUntil("Veuillez spécifier votre nom :", scanner)
+	fmt.Print(reply)
+
+	for reply != cmd.STOP {
+		req, _ = consoleReader.ReadString('\n')
+		fmt.Println("request: " + req)
+		writelnStr(req, &conn)
+		reply = readLine(scanner)
+		fmt.Println("reply : " + reply)
 	}
 }
